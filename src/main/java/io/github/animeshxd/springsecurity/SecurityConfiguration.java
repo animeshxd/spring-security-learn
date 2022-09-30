baccc3f6b17f536c98efc2c2f0d49deb1219e021
Animesh Murmu <am2646374@gmail.com>
2022-09-30 23:11:58 +0530

    Configuring explicit PasswordEncoder - BCryptPasswordEncoder, PasswordEncoderFactories,

--- src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
+++ src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,14 +19,16 @@ public class SecurityConfiguration {
     public UserDetailsService userDetailsService(){
 
         UserDetails user = User
-            .withDefaultPasswordEncoder()
+            .builder()
             .username("user")
             .password("user")
+            .passwordEncoder(passwordEncoder()::encode)
             .roles("USER")
             .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
+        UserDetails admin = User.builder()
             .username("admin")
             .password("admin")
+            .passwordEncoder(passwordEncoder()::encode)
             .roles("ADMIN")
             .build();
         return new InMemoryUserDetailsManager(user, admin);
@@ -39,4 +44,9 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+        // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }

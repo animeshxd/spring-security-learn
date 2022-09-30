297c9fcc7565267b0ba7fd8e3a4cd8211fe8b1e2
Animesh Murmu <am2646374@gmail.com>
2022-09-30 23:05:43 +0530

    Configuring basic Authorization - SecurityFilterChain, HttpSecurity

--- src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
+++ src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package io.github.animeshxd.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -19,7 +21,22 @@ public class SecurityConfiguration {
             .password("user")
             .roles("USER")
             .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("admin")
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests(autz -> autz
+            .antMatchers("/").permitAll()
+            .antMatchers("/admin").hasAnyRole("ADMIN")
+            .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+        ).formLogin();
+        return http.build();
     }
 
 }

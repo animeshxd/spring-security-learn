6bdbb284c7fb44f55b1fc0c6b04feb05188d3ee9
Animesh Murmu <am2646374@gmail.com>
2022-10-01 19:29:14 +0530

    logout support for custom login page

--- src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
+++ src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -49,6 +50,12 @@ public class SecurityConfiguration {
         ).rememberMe(rememberMe -> rememberMe
                 .tokenValiditySeconds(5)
                 .key("some secret key")
+        ).logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID", "remember-me")
         );
         return http.build();
     }

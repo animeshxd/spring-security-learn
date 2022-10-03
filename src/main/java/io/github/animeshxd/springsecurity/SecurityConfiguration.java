d7b063d49e9727e4721d5649013af3a051700f67
Animesh Murmu <am2646374@gmail.com>
2022-10-03 12:39:44 +0530

    init

---
+++ src/main/java/io/github/animeshxd/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package io.github.animeshxd.springsecurity;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfiguration {
+    
+
+}
---
+++ src/main/java/io/github/animeshxd/springsecurity/controller/BasicController.java
@@ -0,0 +1,23 @@
+package io.github.animeshxd.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+    
+    @GetMapping("/")
+    public String root(){
+        return "Welcome everyone";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "Welcome User";
+    }
+    
+    @GetMapping("/admin")
+    public String admin(){
+        return "Welcome Admin";
+    }
+}
